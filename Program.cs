f88ad7514feecba753af9eb674c743ef23f1518d
Milad Hassan Pour <127300802+MiladCodeDev@users.noreply.github.com>
2023-03-09 19:03:15 +0330

    Rectangle Area

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Rectangle Application By. Milad Hassan Pour";
+            double dblL, dblW, dblA, dblP;
+            Console.Write("\n\t Enter Lenght:");
+            dblL = Double.Parse(Console.ReadLine());
+            Console.Write("\n\t Enter Width:");
+            dblW = Double.Parse(Console.ReadLine());
+            dblA = dblL * dblW;
+            dblP = (dblL + dblW) * 2;
+            Console.Write("\n\t Area={0}", dblA);
+            Console.Write("\n\t Perimeter=");
+            Console.Write(dblP);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            Console.Write("\n\t      Length=");
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.Write(dblL);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\n\t ┌────────────────┐");
+            Console.Write("\n\t │                │ ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Width=");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(dblW);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("\n\t │                │ ");
+            Console.Write("\n\t │                │ ");
+            Console.Write("\n\t └────────────────┘");
+
+            Console.ReadKey();
+
+
+        }//Main
+    }//Program
+}//Rectangle
